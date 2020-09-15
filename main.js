d04abe0f165b0c0eeaaa6ebab3dad7313585a956
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-15 15:02:45 -0500

    auth firebase

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ const signupForm = document.querySelector("#signup-form");
 
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(submiting);
+
+  const email = document.querySelector("#signup-email").value;
+  const password = document.querySelector("#signup-password").value;
+
+
+  auth.
+    createUserWithEmailAndPassword(email, password)
 });
